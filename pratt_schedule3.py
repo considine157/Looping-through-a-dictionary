dbc6a04d84e16239a8f66f7865faa74e4b1caa3e
Zane Castillo <58093234+considine157@users.noreply.github.com>
2020-02-10 17:42:36 -0500

    Create pratt_schedule3.py

---
+++ pratt_schedule3.py
@@ -0,0 +1,184 @@
+schedule = {
+	"monday" : {
+
+				612 : [
+						{
+							'class' : 'Human-Information Behavior',
+							'prof'	: 'Pattuelli',
+							'start'	: 6.5
+						},
+						{
+							'class' : 'Information Professions',
+							'prof'	: 'Lopatovska',
+							'start'	: 12.5
+						}
+					],
+				611	:[
+						{
+							'class' : 'Research Mthds/Law Lit',
+							'prof'	: 'Hollander',
+							'start'	: 3.5
+						},
+						{
+							'class' : 'Strategic Leadership',
+							'prof'	: 'Norberg',
+							'start'	: 6.5
+						}
+					],
+				602	:[
+						{
+							'class' : 'Conservation and Preservation',
+							'prof'	: 'Hagadorn',
+							'start'	: 6.5
+						}
+					],
+				609	:[
+						{
+							'class' : 'Info Services & Resources',
+							'prof'	: 'Drabinski',
+							'start'	: 6.5
+						}
+					]
+			},
+
+	"tuesday": {
+
+				609 : [
+
+						{
+							'class' : 'Information Arch/Inter Design',
+							'prof'	: 'MacDonald',
+							'start'	: 3.5
+						},
+						{
+							'class' : 'Information Arch/Inter Design',
+							'prof'	: 'MacDonald',
+							'start'	: 6.5
+						}
+				],
+
+				611 : [
+
+						{
+							'class' : 'Information Professions',
+							'prof'	: 'Berry',
+							'start'	: 6.5
+						}
+				],
+
+				613 : [
+						{
+							'class' : 'Acad Libraries and Scholarly',
+							'prof'	: 'Rabina',
+							'start'	: 6.5
+						},
+						{
+							'class' : 'Info Services & Resources',
+							'prof'	: 'Rabina',
+							'start'	: 3.5
+						}
+				]
+			},
+
+	"wednesday": {
+
+				609 : [
+
+						{
+							'class' : 'Info Services & Resources',
+							'prof'	: 'Kroski',
+							'start'	: 6.5
+						},
+						{
+							'class' : 'Usability Theory & Practice',
+							'prof'	: 'MacDonald',
+							'start'	: 3.5
+						}
+				],
+
+				612 : [
+
+						{
+							'class' : 'Mgmt of Archives/Sp Collection',
+							'prof'	: 'Cucchiara',
+							'start'	: 3.5
+						}
+				],
+
+				613 : [
+						{
+							'class' : 'Government Info Sources',
+							'prof'	: 'Rabina',
+							'start'	: 6.5
+						}
+				],
+
+				410 : [
+						{
+							'class' : 'Library Media Centers',
+							'prof'	: 'Taylor-Swee',
+							'start'	: 3.5
+						}
+				],
+
+				702 : [
+						{
+							'class' : 'Mgmt of Archives/Sp Collection',
+							'prof'	: 'Cucchiara',
+							'start'	: 6.5
+						}
+				]
+			},
+
+	"thursday": {
+
+				611 : [
+
+						{
+							'class' : 'Information Science Research',
+							'prof'	: 'Lopatovska',
+							'start'	: 3.5
+						}
+				],
+
+				612 : [
+						{
+							'class' : 'Information Professions',
+							'prof'	: 'Lopatovska',
+							'start'	: 6.5
+						}
+				],
+
+				'CAMP' : [
+						{
+							'class' : 'Information Professions',
+							'prof'	: 'Sula',
+							'start'	: 3.5
+						}
+				]
+			}
+}
+
+
+
+#loop through each day and then each room and then each class and print it out
+
+# loop for each day.
+for day in schedule: print (day)
+
+#loop for each room.
+for day in schedule:
+    for room in schedule[day]: print (room)
+
+#loop for each class.
+for day in schedule:
+    for room in schedule[day]:
+        for my_class in schedule[day][room]: print (my_class['class'])
+
+
+
+
+
+
+
+
